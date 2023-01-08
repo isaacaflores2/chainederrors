5fe9eeaa436b03e0ab226d3dfd2aa5422aa45af9
isaacaflores2 <isaac.a.flores2@gmail.com>
2023-01-08 15:32:16 -0800

    updated README.md and go doc example

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # chainederrors
-Library which supports error chains so errors.Is() works with nested errors
+Go library which supports error chains so `errors.Is()` works with nested errors. 
+The `Wrap` method is designed to replace `fmt.Errorf` and improve the limitation where the original error wrapped by `fmt.Errorf` is hidden. 
 
 ```go
 var (
@@ -11,12 +12,17 @@ var (
 // wrap errors using fmt
 nestedErr := fmt.Errorf("%w : %s", errReadDocument, errNotFound)
 err := fmt.Errorf("%w : %s", errGetUserInfo, nestedErr)
+
+// original error is hidden
 fmt.Printf("errors.Is(err, errNotFound) = %t \n", errors.Is(err, errNotFound)) // false
 
 // wrap errors using chainederrors
 nestedErr = chainederrors.Wrap(errReadDocument, errNotFound)
 err = chainederrors.Wrap(errGetUserInfo, nestedErr)
+
+// original error is now nested and "checkable"
 fmt.Printf("errors.Is(err, errNotFound) = %t \n", errors.Is(err, errNotFound)) // true
 
+// nested error String() format
 fmt.Printf("err.Error() = %s \n", err.Error()) // get user info : read document : document not found
 ```
--- chainederrors/chainederrors_test.go
+++ chainederrors/chainederrors_test.go
@@ -10,39 +10,39 @@ import (
 
 func TestErrorChain(t *testing.T) {
 	var (
-		errGetItem  = errors.New("get item")
+		errGetItem     = errors.New("get item")
 		errMakeRequest = errors.New("make network request")
-		errTimeout = errors.New("request timeout")
-		errChain = chainederrors.Wrap(errMakeRequest, errTimeout)
+		errTimeout     = errors.New("request timeout")
+		errChain       = chainederrors.Wrap(errMakeRequest, errTimeout)
 	)
 	testCases := []struct {
-		name        string
-		err         error
+		name      string
+		err       error
 		targetErr error
 	}{
 		{
-			name:        "Check for outer error",
-			err:         chainederrors.Wrap(errGetItem, errMakeRequest),
+			name:      "Check for outer error",
+			err:       chainederrors.Wrap(errGetItem, errMakeRequest),
 			targetErr: errGetItem,
 		},
 		{
-			name:        "Check for inner chained error",
-			err:         chainederrors.Wrap(errGetItem, errMakeRequest),
+			name:      "Check for inner chained error",
+			err:       chainederrors.Wrap(errGetItem, errMakeRequest),
 			targetErr: errMakeRequest,
 		},
 		{
-			name:        "Check for error in existing chain",
-			err:         chainederrors.Wrap(errGetItem, errChain),
+			name:      "Check for error in existing chain",
+			err:       chainederrors.Wrap(errGetItem, errChain),
 			targetErr: errTimeout,
 		},
 		{
-			name:        "Outer error is nil",
-			err:         chainederrors.Wrap(nil, errTimeout),
+			name:      "Outer error is nil",
+			err:       chainederrors.Wrap(nil, errTimeout),
 			targetErr: errTimeout,
 		},
 		{
-			name:        "Inner error is nil",
-			err:         chainederrors.Wrap(nil, errTimeout),
+			name:      "Inner error is nil",
+			err:       chainederrors.Wrap(nil, errTimeout),
 			targetErr: errTimeout,
 		},
 	}
@@ -50,10 +50,10 @@ func TestErrorChain(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			x, ok := tc.err.(interface{ Is(error) bool })
-			
+
 			if ok && x.Is(tc.targetErr) {
 				fmt.Println("is target!")
-			}	
+			}
 
 			if isErr := errors.Is(tc.err, tc.targetErr); isErr == false {
 				t.Errorf("errors.Is() got = %t, want true", isErr)
@@ -68,20 +68,22 @@ func TestErrorChain(t *testing.T) {
 
 func ExampleWrap() {
 	var (
-		errGetUserInfo = errors.New("get user info")
+		errGetUserInfo  = errors.New("get user info")
 		errReadDocument = errors.New("read document")
-		errNotFound = errors.New("document not found")
+		errNotFound     = errors.New("document not found")
 	)
 
 	// wrap errors using fmt
 	nestedErr := fmt.Errorf("%w : %s", errReadDocument, errNotFound)
 	err := fmt.Errorf("%w : %s", errGetUserInfo, nestedErr)
-	fmt.Printf("errors.Is(err, errNotFound) = %t \n", errors.Is(err, errNotFound)) // false
+	fmt.Println(errors.Is(err, errNotFound))
+	// Output: false
 
 	// wrap errors using chainederrors
 	nestedErr = chainederrors.Wrap(errReadDocument, errNotFound)
 	err = chainederrors.Wrap(errGetUserInfo, nestedErr)
-	fmt.Printf("errors.Is(err, errNotFound) = %t \n", errors.Is(err, errNotFound)) // true
-	
+	fmt.Println(errors.Is(err, errNotFound)) // true
+	// Output: true
+
 	fmt.Printf("err.Error() = %s \n", err.Error()) // get user info : read document : document not found
 }
